7cf0fa9f141cf09aa036385c7835098259838be4
Ian G <66448861+iangaunt@users.noreply.github.com>
2024-02-01 13:01:16 -0800

    Added new Preact build

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -1,21 +1,33 @@
 import "../css/style.css";
 
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { Icon } from '@iconify/react';
+import { h, render, Component } from "preact";
 
-function Header() {
-    return (
-        <header>
-            <h1>Ian's <Icon className="logo" icon="cib:react" width="100px" height="100px" /> Skeleton</h1>
-            <h2>Feel free to begin editing <span>index.tsx</span>.</h2>
-        </header>
-    )
+class Header extends Component {
+    state = {time: Date.now()};
+    timer: NodeJS.Timeout = null;
+
+    componentDidMount(): void {
+        this.timer = setInterval(() => {
+            this.setState({time: Date.now()});
+        }, 1000);
+    }
+
+    componentWillUnmount(): void {
+        clearInterval(this.timer);
+    }
+
+    render() {
+        const time: string = new Date(this.state.time).toLocaleTimeString();
+
+        return (
+            <header>
+                <h1>Ian's Preact Skeleton</h1>
+                <h2>Feel free to begin editing <span>index.tsx</span>.</h2>
+                <p>The current time is <span>{time}</span>.</p>
+            </header>
+        )
+    }
 }
 
-createRoot(document.getElementById("header")).render(Header());
-
-function hello() {
-    return "Hello!";
-}
-console.log(hello());
+render(<Header />, document.getElementById("header"));
--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,21 @@ const prod = process.env.NODE_ENV === "production";
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+// Establishes the module exports for Webpack to place into ./dist.
 module.exports = {
     mode: prod ? "production" : "development",
     entry: "./src/scripts/index.tsx",
     output: {
         path: __dirname + "/dist/",
     },
+    resolve: {
+        alias: {
+            "react": "preact/compat",
+            "react-dom/test-utils": "preact/test-utils",
+            "react-dom": "preact/compat",
+            "react/jsx-runtime": "preact/jsx-runtime"
+        },
+    },    
     module: {
         rules: [{
                 test: /\.(ts|tsx)$/,
@@ -32,6 +41,7 @@ module.exports = {
     ],
 };
 
+// Add more index files here.
 const files = ["index.html"];
 for (let index in files) {
     module.exports.plugins.push(
